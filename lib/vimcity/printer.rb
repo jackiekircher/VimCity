56bada6ff5b6324fb46fe834ef67f37e96d2ec1b
jackiekircher <kircher.jackie@gmail.com>
2011-11-05 21:40:42 -0400

    change print_to_screen to print_to_buffer

--- lib/vimcity/printer.rb
+++ lib/vimcity/printer.rb
@@ -1,12 +1,12 @@
 module Printer
 
-  def print_to_screen(x, y, str)
+  def print_to_buffer(buffer, x, y, str)
     x = (x < 0) ? 0 : x
     y = (y < 1) ? 1 : y
 
-    new_line = @buffer[y]
+    new_line = buffer[y]
     new_line[x, str.length] = str
-    @buffer[y] = new_line
+    buffer[y] = new_line
     redraw
 
     return str.length
